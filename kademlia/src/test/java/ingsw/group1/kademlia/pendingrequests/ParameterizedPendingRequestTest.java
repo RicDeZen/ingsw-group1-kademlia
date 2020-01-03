a719d3b93460ec73c4699e66643543de175da79e
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2020-01-03 16:19:02 +0100

    Changes after code review by Mariotto and Cestaro https://github.com/turcato-niccolo/SmsHandler/pull/2/files
    (Fixed mistake in KadAction due to change of interface for SMSPeer)

--- kademlia/src/test/java/ingsw/group1/kademlia/pendingrequests/ParameterizedPendingRequestTest.java
+++ kademlia/src/test/java/ingsw/group1/kademlia/pendingrequests/ParameterizedPendingRequestTest.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.robolectric.ParameterizedRobolectricTestRunner;
 
 import java.lang.reflect.Constructor;
